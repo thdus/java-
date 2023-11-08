622c556b47f1e00c21268e2d8a27fc17136284f6
thdus <sykim0659@naver.com>
2023-11-08 22:21:01 +0900

    상수형 데이터 타입

---
+++ src/org/opentutorials/javatutorials/numberstring/ConstantDemo.java
@@ -0,0 +1,12 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class ConstantDemo {
+
+	public static void main(String[] args) {
+		long a = 2147483648L;
+		byte b = 100;
+		short c = 200;
+
+	}
+
+}
