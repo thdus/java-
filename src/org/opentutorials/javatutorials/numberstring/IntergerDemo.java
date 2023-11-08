12ca667b981ff5856bba57d59305a65ec6908c82
thdus <sykim0659@naver.com>
2023-11-08 21:30:41 +0900

    변수의 선언과 할당, 정수와 실수

---
+++ src/org/opentutorials/javatutorials/numberstring/IntergerDemo.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class IntergerDemo {
+
+	public static void main(String[] args) {
+		int a;
+		a = 1;
+		System.out.println(a+1); //2
+		
+		a = 2;
+		System.out.println(a+1); //3
+	}
+
+}
