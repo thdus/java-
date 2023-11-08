595ec197774be499edebf2ddcff07afb191e975c
thdus <sykim0659@naver.com>
2023-11-08 22:09:23 +0900

    데이터의 크기

--- src/org/opentutorials/javatutorials/numberstring/IntDatatypeDemo.java
+++ src/org/opentutorials/javatutorials/numberstring/IntDatatypeDemo.java
@@ -3,7 +3,8 @@ package org.opentutorials.javatutorials.numberstring;
 public class IntDatatypeDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		short a;
+		a = 128;
 
 	}
 
