dea13044e990a9b036a8f63328bab5b61009b366
thdus <sykim0659@naver.com>
2023-11-08 22:56:09 +0900

    연산자

---
+++ src/org/opentutorials/javatutorials/numberstring/ConcatDemo.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class ConcatDemo {
+
+	public static void main(String[] args) {
+		String firstString = "This is";
+		String secondString = "a concatenateds string";
+		String thirdString = firstString+secondString;
+		System.out.println(thirdString);
+
+	}
+
+}
