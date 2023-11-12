c20fe53f5cc97ac2372661b168583f0a41f93c91
thdus <sykim0659@naver.com>
2023-11-12 20:58:17 +0900

    if

--- src/org/opentutorials/javatutorials/conditon/Condition1Demo.java
+++ src/org/opentutorials/javatutorials/conditon/Condition1Demo.java
@@ -3,8 +3,11 @@ package org.opentutorials.javatutorials.conditon;
 public class Condition1Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if(false) {
+			System.out.println("result : true");
+		}
 
 	}
 
 }
+
