58ef3def6db57b4d77f27731c1961a2f07344275
thdus <sykim0659@naver.com>
2023-11-12 23:02:29 +0900

    조건문의 중첩

---
+++ src/org/opentutorials/javatutorials/conditon/LoginDemo2.java
@@ -0,0 +1,20 @@
+package org.opentutorials.javatutorials.conditon;
+
+public class LoginDemo2 {
+
+	public static void main(String[] args) {
+		String id = args[0];
+		String password = args[1];
+		if (id.equals("egoing")) {
+			if (password.equals("111111")) {
+				System.out.println("right");
+			} else {
+				System.out.println("wrong");
+			}
+		} else {
+			System.out.println("wrong");
+		}
+
+	}
+
+}
