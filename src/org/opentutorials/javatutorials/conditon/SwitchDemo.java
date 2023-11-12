c0898e47285194e65005ed74e7f0b1e2107a727d
thdus <sykim0659@naver.com>
2023-11-12 23:14:41 +0900

    switch

---
+++ src/org/opentutorials/javatutorials/conditon/SwitchDemo.java
@@ -0,0 +1,41 @@
+package org.opentutorials.javatutorials.conditon;
+
+public class SwitchDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("switch(1)");
+		switch(1) {
+		case 1:
+			System.out.println("one");
+		case 2:
+			System.out.println("two");
+		case 3:
+			System.out.println("three");
+		}
+		
+
+		System.out.println("switch(2)");
+		switch(2) {
+		case 1:
+			System.out.println("one");
+		case 2:
+			System.out.println("two");
+		case 3:
+			System.out.println("three");
+		}
+		
+
+		System.out.println("switch(3)");
+		switch(3) {
+		case 1:
+			System.out.println("one");
+		case 2:
+			System.out.println("two");
+		case 3:
+			System.out.println("three");
+		}
+
+	}
+
+}
