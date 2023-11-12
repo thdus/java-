bf28b86d9329051712d80d4701898c3b8456cfec
thdus <sykim0659@naver.com>
2023-11-12 23:15:05 +0900

    switch

--- src/org/opentutorials/javatutorials/conditon/SwitchDemo.java
+++ src/org/opentutorials/javatutorials/conditon/SwitchDemo.java
@@ -8,9 +8,23 @@ public class SwitchDemo {
 		switch(1) {
 		case 1:
 			System.out.println("one");
+			break;
 		case 2:
 			System.out.println("two");
+			break;
 		case 3:
+			System.out.println("three");
+			break;
+		default:
+			System.out.println("default");
+		}
+		
+		int val = 1;
+		if(val ==1) {
+			System.out.println("one");
+		} else if(val == 2) {
+			System.out.println("two");
+		} else if(val == 3) {
 			System.out.println("three");
 		}
 		
