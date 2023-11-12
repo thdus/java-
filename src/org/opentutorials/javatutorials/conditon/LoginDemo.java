004d1df176366c7d673633cd9881ef7cea3438cd
thdus <sykim0659@naver.com>
2023-11-12 22:42:50 +0900

    조건문의 응용

--- src/org/opentutorials/javatutorials/conditon/LoginDemo.java
+++ src/org/opentutorials/javatutorials/conditon/LoginDemo.java
@@ -3,7 +3,12 @@ package org.opentutorials.javatutorials.conditon;
 public class LoginDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String id = args[0];
+		if(id.equals("egoing")){
+			System.out.println("right");
+		} else {
+			System.out.println("wrong");
+		}
 
 	}
 
