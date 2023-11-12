cfb0dfded08ff7d562b2d3cc399009cebe3638bf
thdus <sykim0659@naver.com>
2023-11-12 21:37:56 +0900

    else

--- src/org/opentutorials/javatutorials/conditon/Condition3Demo.java
+++ src/org/opentutorials/javatutorials/conditon/Condition3Demo.java
@@ -3,7 +3,11 @@ package org.opentutorials.javatutorials.conditon;
 public class Condition3Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if (true) {
+			System.out.println(1);
+		} else {
+			System.out.println(2);
+		}
 
 	}
 
--- src/org/opentutorials/javatutorials/conditon/ElseDemo.java
+++ src/org/opentutorials/javatutorials/conditon/ElseDemo.java
@@ -3,7 +3,15 @@ package org.opentutorials.javatutorials.conditon;
 public class ElseDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if (false) {
+			System.out.println(1);
+		}	else if (true) {
+				System.out.println(2);
+		}	else if (true) {
+				System.out.println(3);
+		}	else {
+				System.out.println(4);
+		}
 
 	}
 
