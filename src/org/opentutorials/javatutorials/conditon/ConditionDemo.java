31d76ec942d2885ac56c21a452a2e96b4128e2f2
thdus <sykim0659@naver.com>
2023-11-12 20:55:24 +0900

    if

---
+++ src/org/opentutorials/javatutorials/conditon/ConditionDemo.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.conditon;
+
+public class ConditionDemo {
+
+	public static void main(String[] args) {
+		if(true) {
+			System.out.println("result : true");
+		}
+	}
+
+}
--- src/org/opentutorioals/javatutorials/compare/EqualDemo.java
+++ src/org/opentutorioals/javatutorials/compare/EqualDemo.java
@@ -3,7 +3,10 @@ package org.opentutorioals.javatutorials.compare;
 public class EqualDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(1 == 2); //false
+		System.out.println(1 == 1); //true
+		System.out.println("one" == "two"); // false
+		System.out.println("one" == "one"); // true
 
 	}
 
