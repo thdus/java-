773b5d8324b5ed98e6da07a0ade67d20d8a1214f
thdus <sykim0659@naver.com>
2023-11-28 21:14:03 +0900

    반복문:while

--- src/org/opentutorials/javatutorials/loop/WhileDemo.java
+++ src/org/opentutorials/javatutorials/loop/WhileDemo.java
@@ -3,8 +3,9 @@ package org.opentutorials.javatutorials.loop;
 public class WhileDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		while (true) {
+			System.out.println("Coding Everybody");
+		}
 	}
 
 }
