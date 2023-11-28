e4a7f6b6e3c12b0ae4f336430017ea6fe889c173
thdus <sykim0659@naver.com>
2023-11-28 22:13:51 +0900

    배열:정의

--- src/org/opentutorials/javatutorials/array/DefineDemo.java
+++ src/org/opentutorials/javatutorials/array/DefineDemo.java
@@ -3,8 +3,7 @@ package org.opentutorials.javatutorials.array;
 public class DefineDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[] classGroup = { "최진혁", "최유빈", "한아람", "이고잉" };
 	}
 
 }
