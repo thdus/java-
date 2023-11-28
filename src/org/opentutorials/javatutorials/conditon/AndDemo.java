8ac1527b8dcc25d33d6631e763969bc8ae494739
thdus <sykim0659@naver.com>
2023-11-28 20:39:58 +0900

    논리연산자 : and

---
+++ src/org/opentutorials/javatutorials/conditon/AndDemo.java
@@ -0,0 +1,23 @@
+package org.opentutorials.javatutorials.conditon;
+
+public class AndDemo {
+
+	public static void main(String[] args) {
+		if (true && true) {
+			System.out.println(1);
+		}
+		
+		if (true && false ) {
+			System.out.print(2);
+			
+		if (false && true) {
+			System.out.println(3);
+		}
+		
+		if (false && false) {
+			System.out.println(4);
+		}
+		}
+	}
+
+}
---
+++ src/org/opentutorials/javatutorials/conditon/LoginDemo3.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.conditon;
+
+public class LoginDemo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
