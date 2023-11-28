225ca879bfdaa927731b9630618b57cf72f8edb5
thdus <sykim0659@naver.com>
2023-11-28 20:42:32 +0900

    논리연산자 : and

--- src/org/opentutorials/javatutorials/conditon/LoginDemo3.java
+++ src/org/opentutorials/javatutorials/conditon/LoginDemo3.java
@@ -3,8 +3,13 @@ package org.opentutorials.javatutorials.conditon;
 public class LoginDemo3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String id = args[0];
+		String password = args[1];
+		if (id.equals("egoing") && password.equals("111111")) {
+			System.out.println("right");
+		}	else {
+			System.out.println("wrong");
+		}
 	}
 
 }
