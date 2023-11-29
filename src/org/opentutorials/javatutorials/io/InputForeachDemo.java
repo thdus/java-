e59845484af7ca2313f27b757f95d53814fbdbc9
thdus <sykim0659@naver.com>
2023-11-29 17:11:28 +0900

    앱이 시작할 때 데이터를 입력:2

--- src/org/opentutorials/javatutorials/io/InputForeachDemo.java
+++ src/org/opentutorials/javatutorials/io/InputForeachDemo.java
@@ -1,9 +1,11 @@
 package org.opentutorials.javatutorials.io;
 
-public class InputDemo {
+public class InputForeachDemo {
 
 	public static void main(String[] args) {
-		System.out.println(args.length);
+		for(String e : args) {
+			System.out.println(e);
+		}
 	}
 
 }
