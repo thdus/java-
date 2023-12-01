1c8f482714726e6938a09c0463b3ecd5dfb54754
thdus <sykim0659@naver.com>
2023-12-01 15:53:57 +0900

    패키지:패키지사용

---
+++ src/org/opentutorials/javatutorials/packages/example1/B.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.packages.example1;
+
+public class B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/org/opentutorials/javatutorials/packages/example2/B.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.packages.example2;
+
+public class B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
