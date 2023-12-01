bd8879128eb9f275c324d106111a5d3ffec0e1b5
thdus <sykim0659@naver.com>
2023-12-01 15:54:34 +0900

    패키지:패키지사용

--- src/org/opentutorials/javatutorials/packages/example1/B.java
+++ src/org/opentutorials/javatutorials/packages/example1/B.java
@@ -3,7 +3,7 @@ package org.opentutorials.javatutorials.packages.example1;
 public class B {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		A a = new A();
 
 	}
 
