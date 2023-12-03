9c067d08d31c2d290c937fa3b39bfeb8517d80e5
sykim <sykim@10.50.44.151>
2023-12-03 17:41:49 +0900

    예외:예외 사슬

---
+++ src/org/opentutorials/javatutorials/exception/ThrowExceptionDemo.java
@@ -0,0 +1,21 @@
+package org.opentutorials.javatutorials.exception;
+
+class B{
+	void run() {
+	}
+}
+class C{
+	void run() {
+		B b = new B();
+		b.run();
+	}
+}
+public class ThrowExceptionDemo {
+
+	public static void main(String[] args) {
+		C c = new c();
+		c.run();
+
+	}
+
+}
