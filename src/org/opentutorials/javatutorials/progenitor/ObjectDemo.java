8060eac7dbae7ebb6b81e878ed13a2d03819ad05
sykim <sykim@10.50.44.151>
2023-12-03 22:37:24 +0900

    Object 클래스:clone

--- src/org/opentutorials/javatutorials/progenitor/ObjectDemo.java
+++ src/org/opentutorials/javatutorials/progenitor/ObjectDemo.java
@@ -1,24 +1,26 @@
 package org.opentutorials.javatutorials.progenitor;
 
-class Student{
+class Student implements Cloneable{
 	String name;
 	Student(String name){
 		this.name = name;
 	}
-	public boolean equals(Object obj) {
-		Student s = (Student)obj;
-		return this.name == s.name;
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
 	}
 }
 public class ObjectDemo {
 
 	public static void main(String[] args) {
 		Student s1 = new Student("egoing");
-		Student s2 = new Student("egoing");
-		System.out.println(s1 == s2);
-		System.out.println(s1.equals(s2));
-		
-
+		try {
+			Student s2 = (Student)s1.clone();
+			System.out.println(s1.name);
+			System.out.println(s2.name);
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
