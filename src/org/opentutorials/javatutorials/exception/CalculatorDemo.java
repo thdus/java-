10697fac745d0b209886a3f5756c044153bdb8d4
sykim <sykim@10.50.44.151>
2023-12-03 19:42:07 +0900

    예외:예외 만들기

--- src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
+++ src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
@@ -6,6 +6,9 @@ class Calculator{
 		this.right = right;
 	}
 	public void divide() {
+		if(right == 0) {
+			throw new ArithmeticException("0으로 나눌 수 없습니다");
+		}
 		try {
 			System.out.println("계산결과는 ");
 			System.out.println(this.left/this.right);
@@ -21,7 +24,11 @@ public class CalculatorDemo {
 	public static void main(String[] args) {
 		Calculator c1 =new Calculator();
 		c1.setOprands(10, 0);
-		c1.divide();
+		try {
+			c1.divide();	
+		} catch(ArithmeticException e) {
+			System.out.println(e.getMessage());
+		}
 		
 		Calculator c2 = new Calculator();
 		c2.setOprands(10, 5);
