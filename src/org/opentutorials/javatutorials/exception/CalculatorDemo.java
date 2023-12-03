11f505c8dd50ecf7c8f4486712a557e3c2b4bbc5
sykim <sykim@10.50.44.151>
2023-12-03 21:07:22 +0900

    object클래스:toString

--- src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
+++ src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
@@ -1,21 +1,32 @@
 package org.opentutorials.javatutorials.exception;
+
+class DivideException extends Exception{
+	public int left;
+	public int right;
+	DivideException(){
+		super();
+	}
+	DivideException(String message){
+		super(message);
+    }
+	DivideException(String message, int left, int right){
+		super(message);
+		this.left = left;
+		this.right = right;
+	}
+}
 class Calculator{
 	int left, right;
 	public void setOprands(int left, int right){
 		this.left = left;
 		this.right = right;
 	}
-	public void divide() {
+	public void divide() throws DivideException {
 		if(right == 0) {
-			throw new ArithmeticException("0으로 나눌 수 없습니다");
+			throw new DivideException("0으로 나눌 수 없습니다");
 		}
-		try {
-			System.out.println("계산결과는 ");
-			System.out.println(this.left/this.right);
-			System.out.println(" 입니다 ");
-		} catch(Exception e) {
-			System.out.println("오류가 발생했습니다 : "+e.getMessage());
-		}
+		System.out.print(this.left/this.right);
+		
 	}
 }
 
@@ -26,14 +37,11 @@ public class CalculatorDemo {
 		c1.setOprands(10, 0);
 		try {
 			c1.divide();	
-		} catch(ArithmeticException e) {
+		} catch(DivideException e) {
 			System.out.println(e.getMessage());
+			System.out.println(e.left);
+			System.out.println(e.right);
 		}
-		
-		Calculator c2 = new Calculator();
-		c2.setOprands(10, 5);
-		c2.divide();
-
 	}
 
 }
